03bcd9354a42ed41c67e47de248f9b00cf2a020b
UmerZia <omerzia73@gmail.com>
2022-09-14 11:16:28 +0500

    fix eslint error

--- js/script.js
+++ js/script.js
@@ -3,26 +3,25 @@ const hamburgerIcon = document.getElementById('hamburger');
 const crossIcon = document.getElementById('cross-icon');
 const navLinks = document.getElementsByClassName('navbar__desktop-icon');
 
-
 hamburgerIcon.addEventListener('click', () => {
-    document.getElementsByClassName('navbar__content')[0].classList.add("display-flex");
-    hamburgerIcon.classList.add("display-none");
+  document.getElementsByClassName('navbar__content')[0].classList.add('display-flex');
+  hamburgerIcon.classList.add('display-none');
 });
 
 crossIcon.addEventListener('click', () => {
-    document.getElementsByClassName('navbar__content')[0].classList.remove("display-flex");
-    hamburgerIcon.classList.remove("display-none");
+  document.getElementsByClassName('navbar__content')[0].classList.remove('display-flex');
+  hamburgerIcon.classList.remove('display-none');
 });
 
-[...navLinks].forEach(navLink => {
-    navLink.addEventListener('click', () => {
-        document.getElementsByClassName('navbar__content')[0].classList.remove("display-flex");
-    });
+[...navLinks].forEach((navLink) => {
+  navLink.addEventListener('click', () => {
+    document.getElementsByClassName('navbar__content')[0].classList.remove('display-flex');
+  });
 });
 
-//Close Hamburger if user switch to desktop version
+// Close Hamburger if user switch to desktop version
 window.addEventListener('resize', () => {
-   if(window.innerWidth >= 768){
-       document.getElementsByClassName('navbar__content')[0].classList.remove("display-flex");
-   }
+  if (window.innerWidth >= 768) {
+    document.getElementsByClassName('navbar__content')[0].classList.remove('display-flex');
+  }
 });
