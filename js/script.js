a042b87c40bc568b51e0e711ac7b244992240b28
UmerZia <omerzia73@gmail.com>
2022-09-27 12:16:58 +0500

    Fix local storage issue of email

--- js/script.js
+++ js/script.js
@@ -66,7 +66,7 @@ const projects = {
       desc: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
       popUp_desc:
                 "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.",
-      tech: ['Git', 'GitHub', 'GitPage'],
+      tech: ['Git', 'GH', 'GitPage'],
       img: '../img/cards/popUp-img.png',
       live: '#',
       source: 'https://github.com/UmerZia/Portfolio',
@@ -253,7 +253,7 @@ userMsg.addEventListener('input', () => {
 window.addEventListener('DOMContentLoaded', () => {
   formData = JSON.parse(localStorage.getItem('formData') || '{}');
   if (formData.email) {
-    document.getElementById('email').value = formData.email;
+    document.getElementById('email').value = formData.email.name;
   }
   if (formData.username) {
     document.getElementById('name').value = formData.username;
